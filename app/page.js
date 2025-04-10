c7a679f571e9835208d405292062a9737be9e1f3
Bassey-coder <godswillimesam@gmail.com>
2025-04-10 11:17:45 +0100

    second commit

--- app/page.js
+++ app/page.js
@@ -1,7 +1,26 @@
-import Image from "next/image";
+"use client"
+import { useEffect, useState } from "react"
+
+
+
 
 export default function Home() {
+  const [count,setCount] = useState(0)
+
+   useEffect(() => {
+    const getData = async () =>{
+      const response = await fetch("https://dummyjson.com/products")
+      const data = await response.json()
+      console.log(data.products)
+    }
+    getData()
+    
+   })
   return (
-   <main>landing page</main>
-  );
+   <main className=" flex flex-col gap-20 items-center justify-center px-20 py-20">
+   <p className="text-4xl font-bold">count : {count}</p>
+   <button onClick={()  => setCount(count +1 )} className="w-[200px] h-[50px] text-white bg-blue-400">increment</button>
+   <button onClick={() => setCount(count - 1)} className="w-[200px] h-[50px] text-white bg-blue-500">Decrement</button>
+   </main>
+  )
 }
